7c53785ab551911c778bc506e2860188c772f7bd
zhovner <denys.zhovnerovych@gmail.com>
2019-03-31 23:49:56 +0300

    Scenario outline

--- src/test/java/implementation/Checkout.java
+++ src/test/java/implementation/Checkout.java
@@ -3,11 +3,13 @@ package implementation;
 import java.util.ArrayList;
 
 public class Checkout {
-    public void add(int count, int price) {
+    private int runningTotal = 0;
 
+    public void add(int count, int price) {
+        runningTotal += (count * price);
     }
 
-    public int total(int total) {
-        return total;
+    public int total() {
+        return runningTotal;
     }
 }
